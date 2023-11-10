7a3d78cf9e374a50a244655612eb00bd7f827a22
Nabela2203 <139353758+Nabela2203@users.noreply.github.com>
2023-11-10 18:27:11 +0530

    Task-10 Uploaded

---
+++ Task-10/question-3.py
@@ -0,0 +1,20 @@
+# 3.) Visit the url https://github.com/rvsp/typescript-oops/blob/master/Practice/music-player.md and
+# convert the UML diagram into Python Class and Methods.
+
+class Practice:
+    def mini_task(self):
+        pass
+    def cutomer_invoice(self):
+        pass
+    def movie(self):
+        pass
+    def tv_class(self):
+        pass
+    def class_circle(self):
+        pass
+    def ecommerce(self):
+        pass
+    def music_player(self):
+        pass
+
+obj=Practice()
